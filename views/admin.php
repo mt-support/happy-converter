866bd1ee728e2f2ed2dfc69f64a025bd75e70c71
Crisoforo Gaspar <hello@crisoforo.com>
2020-03-30 15:50:55 -0600

    Update views to render the settings on the admin
    - Change short syntax using `<?php` instead.
    - Render notice there are non events to process.
    
    EXT-153 #resolve

--- views/admin.php
+++ views/admin.php
@@ -7,11 +7,10 @@
 ?>
 <div class="wrap tec-happy-converter">
 	<h2>The Events Calendar | Happy Converter</h2>
-
 	<div class="notice">
 		<p>
-			<?=
-				sprintf(
+			<?php
+				printf(
 					esc_html(
 						/* Translator note: placeholders %1$s and %2$s represent opening and closing 'strong' tags, respectively. */
 						__( 'This tool is provided in the hope that it will be useful, but comes with no guarantees. %1$sYou%2$s are responsible for the safety of your data. %1$sMake a backup first!%2$s ', 'tec-happy-converter' )
@@ -24,37 +23,37 @@
 	</div>
 	<?php foreach ( $converters as $converter ): ?>
 		<div
-			class="converter <?= $converter->unconverted_data_exists() ? 'active' : 'inactive'; ?>"
-			id="<?= esc_attr( $converter->get_id() ); ?>"
+			class="converter <?php echo esc_attr( $converter->unconverted_data_exists() ? 'active' : 'inactive' ); ?>"
+			id="<?php echo esc_attr( $converter->get_id() ); ?>"
 		>
-			<h4><?= esc_html( $converter->get_name() ); ?></h4>
+			<h4><?php echo esc_html( $converter->get_name() ); ?></h4>
 
 			<div class="counts">
 				<span class="total-nodes">
 					<?php esc_html_e( 'Total nodes:', 'tec-happy-converter' ); ?>
 					<span class="count-value">
-						<?= esc_html( $converter->count_all_data_nodes() ); ?>
+						<?php esc_html_e( $converter->count_all_data_nodes() ); ?>
 					</span>
 				</span>
 
 				<span class="converted-nodes">
 					<?php esc_html_e( 'Converted:', 'tec-happy-converter' ); ?>
 					<span class="count-value">
-						<?= esc_html( $converter->count_converted_data_nodes() ); ?>
+						<?php esc_html_e( $converter->count_converted_data_nodes() ); ?>
 					</span>
 				</span>
 
 				<span class="unconverted-nodes">
 					<?php esc_html_e( 'Unconverted:', 'tec-happy-converter' ); ?>
 					<span class="count-value">
-						<?= esc_html( $converter->count_unconverted_data_nodes() ); ?>
+						<?php esc_html_e( $converter->count_unconverted_data_nodes() ); ?>
 					</span>
 				</span>
 			</div>
 
 			<div class="tools">
 				<button class="run-converter button-secondary">
-					<?= esc_html_x( 'Convert', 'Convert button', 'tec-happy-converter' ); ?>
+					<?php echo esc_html_x( 'Convert', 'Convert button', 'tec-happy-converter' ); ?>
 				</button>
 
 				<div class="progress-indicator">
@@ -62,15 +61,17 @@
 				</div>
 			</div>
 
-			<div class="flag">
-
-			</div>
+			<?php if ( $converter->count_unconverted_data_nodes() === 0) : ?>
+				<div class="flag success">Job complete!</div>
+			<?php else: ?>
+				<div class="flag"></div>
+			<?php endif; ?>
 		</div>
 	<?php endforeach; ?>
 
 	<?php if ( empty( $converters ) ): ?>
 		<p>
-			<?= esc_html( __( 'Sorry, no converters are available.', 'tec-happy-converter' ) ) ?>
+			<?php esc_html_e( __( 'Sorry, no converters are available.', 'tec-happy-converter' ) ) ?>
 		</p>
 	<?php endif; ?>
 </div>
